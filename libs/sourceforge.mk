17546f6afc09750dc4ad45546e048bb72747092b
per_h <per_h@df36aa3e-fc22-4cfe-aecd-28ffda6efaf1>
2011-05-01 12:51:00 +0000

    rename readme file, fixed sourceforge upload

--- libs/sourceforge.mk
+++ libs/sourceforge.mk
@@ -1,7 +1,7 @@
 # Username at SourceForge, used when publishing releases
 USR := per_h
-URL_SVN_SCP := $(USR),dhcp-discover@frs.sourceforge.net:/home/frs/project/d/dh/dhcp-discover/dhcp-discover
+URL_SVN_SCP := $(USR),dhcp-discover@frs.sourceforge.net:/home/frs/project/d/dh/dhcp-discover
 
 # Rule that publish a release at SourceForge
-release2sourceforge: $(REL).tar.bz2
+release2sourceforge: $(REL).tar.bz2 README.txt
 	scp $< $(URL_SVN_SCP)
