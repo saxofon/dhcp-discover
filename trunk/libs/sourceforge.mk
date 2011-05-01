9dbb0c593e2e3461839d12e1b7f637e407d2b4bf
per_h <per_h@df36aa3e-fc22-4cfe-aecd-28ffda6efaf1>
2011-05-01 12:16:50 +0000

    initial import

---
+++ trunk/libs/sourceforge.mk
@@ -0,0 +1,7 @@
+# Username at SourceForge, used when publishing releases
+USR := per_h
+URL_SVN_SCP := $(USR),dhcp-discover@frs.sourceforge.net:/home/frs/project/d/dh/dhcp-discover/dhcp-discover
+
+# Rule that publish a release at SourceForge
+release2sourceforge: $(REL).tar.bz2
+	scp $< $(URL_SVN_SCP)
